c66ff38ffc73e92be23e3e2453f7de1767f914f9
danialeresbar <danialeres@hotmail.com>
2020-06-30 23:38:36 -0500

    fix

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -27,4 +27,4 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
-] +static(settings.static.STATIC_URL, document_root=settings.STATIC_ROOT)
+] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
